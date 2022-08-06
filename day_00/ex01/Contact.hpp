06c9dbfb67886f74b79f79029227c9badc78f4a3
ELENA ZVEREVA <lenazve1996@gmail.com>
2022-08-06 23:27:28 +0300

    fix checkstyle

--- day_00/ex01/Contact.hpp
+++ day_00/ex01/Contact.hpp
@@ -8,16 +8,16 @@
 class Contact {
 
 public:
-	void setFirstName(std::string str);
-	void setLastName(std::string str);
-	void setNickName(std::string str);
-	void setPhoneNumber(std::string str);
-	void setDarkestSecret(std::string str);
-	std::string  getFirstName();
-    std::string getLastName();
-	std::string getNickName();
-	std::string getPhoneNumber();
-	std::string getDarkestSecret();
+	void	setFirstName(std::string str);
+	void	setLastName(std::string str);
+	void	setNickName(std::string str);
+	void	setPhoneNumber(std::string str);
+	void	setDarkestSecret(std::string str);
+	std::string	getFirstName();
+    std::string	getLastName();
+	std::string	getNickName();
+	std::string	getPhoneNumber();
+	std::string	getDarkestSecret();
 
 private:
 	std::string first_name;
